bb6b5a757742ebd6575976a38c09784c829cd034
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2020-02-19 19:08:44 +0100

    refactor: cleanup current implementation

--- index.js
+++ index.js
@@ -5,149 +5,36 @@ require("dotenv").config();
 require("make-promises-safe");
 
 // Require Node.js Dependencies
-const { join, dirname, basename } = require("path");
-const fs = require("fs");
-const { mkdir, writeFile, rmdir } = require("fs").promises;
+const { join } = require("path");
+const { mkdir, rmdir } = require("fs").promises;
 
 // Require Third-party Dependencies
-const Lock = require("@slimio/lock");
+const { cyan, white } = require("kleur");
 const Spinner = require("@slimio/async-cli-spinner");
-const git = require("isomorphic-git");
-const { from, cwd } = require("nsecure");
-const { cyan, white, grey } = require("kleur");
+Spinner.DEFAULT_SPINNER = "dots";
 
 // Require Internal Dependencies
-const { linkPackages, stats } = require("./src/utils");
-
-// Vars
-const token = process.env.GIT_TOKEN;
-const securityLock = new Lock({ maxConcurrent: 2 });
-Spinner.DEFAULT_SPINNER = "dots";
-git.plugins.set("fs", fs);
+const { linkPackages, stats, cloneGITRepository, nsecure } = require("./src/utils");
+const config = require("./data/config.json");
 
 // CONSTANTS
-const ORGA_URL = "https://github.com/SlimIO";
 const CLONE_DIR = join(__dirname, "clones");
 const JSON_DIR = join(__dirname, "json");
-const NPM_ADDONS = [
-    "@slimio/addon",
-    "@slimio/scheduler",
-    "@slimio/config",
-    "@slimio/core",
-    "@slimio/arg-parser",
-    "@slimio/profiles",
-    "@slimio/queue",
-    "@slimio/sqlite-transaction",
-    "@slimio/alert",
-    "@slimio/metrics",
-    "@slimio/units",
-    "@slimio/ipc",
-    "@slimio/safe-emitter"
-];
 
-const BUILTIN_ADDONS = [
-    "Aggregator",
-    "Alerting",
-    "Socket",
-    "Gate"
-];
-
-/**
- * @async
- * @function cloneRep
- * @param {!string} repName
- * @returns {Promise<string>}
- */
-async function cloneRep(repName) {
-    const dir = join(CLONE_DIR, repName);
-    const url = `${ORGA_URL}/${repName}`;
-
-    await git.clone({
-        dir, url, token,
-        singleBranch: true,
-        oauth2format: "github"
-    });
-
-    return dir;
-}
-
-/**
- * @async
- * @function runSecure
- * @param {!string} packageName
- * @returns {Promise<string>}
- */
-async function runSecurePackage(packageName) {
-    await securityLock.acquireOne();
-
-    try {
-        const name = `${packageName}.json`;
-        const payload = await from(packageName, {
-            maxDepth: 4, verbose: false
-        });
-
-        const result = JSON.stringify(Object.fromEntries(payload), null, 2);
-        const filePath = join(JSON_DIR, name);
-        await mkdir(dirname(filePath), { recursive: true });
-        await writeFile(filePath, result);
-
-        return filePath;
-    }
-    catch (error) {
-        return null;
-    }
-    finally {
-        securityLock.freeOne();
-    }
-}
-
-/**
- * @async
- * @function runSecureDir
- * @param {!string} dir
- * @returns {Promise<string>}
- */
-async function runSecureDir(dir) {
-    await securityLock.acquireOne();
-
-    try {
-        const name = `${basename(dir)}.json`;
-        const payload = await cwd(dir, {
-            maxDepth: 4, verbose: false
-        });
-
-        const result = JSON.stringify(Object.fromEntries(payload), null, 2);
-        const filePath = join(JSON_DIR, name);
-        await writeFile(filePath, result);
-
-        return filePath;
-    }
-    catch (error) {
-        return null;
-    }
-    finally {
-        securityLock.freeOne();
-    }
-}
-
-/**
- * @async
- * @function fetchPackagesStats
- * @returns {Map<string, any>}
- */
 async function fetchPackagesStats() {
     const spinner = new Spinner({
         prefixText: white().bold("Fetching packages stats on nsecure")
     }).start();
 
     try {
-        const jsonFiles = await Promise.all(NPM_ADDONS.map(runSecurePackage));
+        const jsonFiles = await Promise.all(config.ADDONS.map(nsecure.onPackage));
 
         const elapsed = `${spinner.elapsedTime.toFixed(2)}ms`;
         spinner.succeed(`Successfully done in ${cyan().bold(elapsed)}`);
         console.log("");
+        const nMap = await linkPackages(jsonFiles);
 
-        return linkPackages(jsonFiles);
+        return stats(nMap);
     }
     catch (error) {
         spinner.failed(error.message);
@@ -155,64 +42,41 @@ async function fetchPackagesStats() {
     }
 }
 
-/**
- * @async
- * @function main
- */
+async function fetchRepositoriesStats() {
+    const spinner = new Spinner({
+        prefixText: white().bold("Clone and analyze built-in addons")
+    }).start("clone repositories...");
+
+    try {
+        const repos = await Promise.all(config.REPOS.map(cloneGITRepository));
+        spinner.text = "Run node-secure analyze";
+
+        const jsonFiles = await Promise.all(repos.map(nsecure.onLocalDirectory));
+        const nMap = await linkPackages(jsonFiles.filter((value) => value !== null));
+        spinner.succeed(`Successfully done in ${spinner.elapsedTime.toFixed(2)}ms`);
+
+        return stats(nMap);
+    }
+    catch (error) {
+        spinner.failed(error.message);
+        throw error;
+    }
+}
+
 async function main() {
     await Promise.all([
         mkdir(JSON_DIR, { recursive: true }),
         mkdir(CLONE_DIR, { recursive: true })
     ]);
 
-    const spinner = new Spinner({
-        prefixText: white().bold("Clone and analyze built-in addons")
-    }).start("clone repositories...");
-
-    let builtInThird;
     try {
-        const repos = await Promise.all(BUILTIN_ADDONS.map(cloneRep));
-        spinner.text = "Run node-secure analyze";
+        const pkgStats = await fetchPackagesStats();
+        const repoStats = await fetchRepositoriesStats();
 
-        const jsonFiles = await Promise.all(repos.map(runSecureDir));
-
-        const nMap = await linkPackages(jsonFiles.filter((value) => value !== null));
-        const fStats = stats(nMap);
-        spinner.succeed(`Successfully done in ${spinner.elapsedTime.toFixed(2)}ms`);
-
-        console.log(grey().bold("\n----------------------------------"));
-        console.log(white().bold("SlimIO built-in stats:\n"));
-        console.log(`Number of Third-party npm packages: ${cyan().bold(fStats.external)}`);
-        // eslint-disable-next-line prefer-template
-        console.log(" - " + [...fStats.third].join("\n - "));
-        builtInThird = fStats.third;
-
-        console.log(`\nNumber of packages with transitive dependencies: ${cyan().bold(fStats.transitive.size)}`);
-        // eslint-disable-next-line prefer-template
-        console.log(" - " + [...fStats.transitive].join("\n - "));
-        console.log("\n");
-    }
-    catch (error) {
-        spinner.failed(error.message);
-        throw error;
+        console.log(JSON.stringify(pkgStats, null, 4));
     }
     finally {
         await rmdir(CLONE_DIR, { recursive: true });
     }
-
-    const pkgStats = await fetchPackagesStats();
-    const fStats = stats(pkgStats);
-
-    console.log(grey().bold("----------------------------------"));
-    console.log(white().bold("SlimIO packages stats:\n"));
-    console.log(`Number of SlimIO npm packages: ${cyan().bold(fStats.internal)}`);
-    console.log(`Number of Third-party npm packages: ${cyan().bold(fStats.external)}`);
-    console.log("List (with dedup filtered):");
-    // eslint-disable-next-line prefer-template
-    console.log(" - " + [...fStats.third].filter((name) => !builtInThird.has(name)).join("\n - "));
-
-    console.log(`\nNumber of packages with transitive dependencies: ${cyan().bold(fStats.transitive.size)}`);
-    // eslint-disable-next-line prefer-template
-    console.log(" - " + [...fStats.transitive].join("\n - "));
 }
 main().catch(console.error);
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,28 @@
 "use strict";
 
 // Require Node.js Dependencies
-const { readFile } = require("fs").promises;
+const { join, dirname, basename } = require("path");
+const fs = require("fs");
+const { mkdir, writeFile, readFile } = require("fs").promises;
 
 // Require Third-party Dependencies
+const Lock = require("@slimio/lock");
+const git = require("isomorphic-git");
+const { from, cwd } = require("nsecure");
 const { yellow } = require("kleur");
+
+// Require Internal Dependencies
+const config = require("../data/config.json");
+
+// CONSTANTS
+const ORGA_URL = `https://github.com/${config.ORG}`;
+const CLONE_DIR = join(__dirname, "..", "clones");
+const JSON_DIR = join(__dirname, "..", "json");
+
+// VARS
+const token = process.env.GIT_TOKEN;
+const securityLock = new Lock({ maxConcurrent: 2 });
+git.plugins.set("fs", fs);
 
 /**
  * @async
@@ -57,6 +75,7 @@ async function linkPackages(files) {
 /**
  * @function stats
  * @param {*} stats
+ * @returns {object}
  */
 function stats(stats) {
     const ref = { internal: 0, external: 0 };
@@ -81,4 +100,90 @@ function stats(stats) {
     return Object.assign({ third, transitive }, ref);
 }
 
-module.exports = { linkPackages, stats };
+/**
+ * @async
+ * @function cloneGITRepository
+ * @description clone a given repository from github
+ * @param {!string} repositoryName
+ * @returns {Promise<string>}
+ */
+async function cloneGITRepository(repositoryName) {
+    const dir = join(CLONE_DIR, repositoryName);
+    const url = `${ORGA_URL}/${repositoryName}`;
+
+    await git.clone({
+        dir, url, token, singleBranch: true, oauth2format: "github"
+    });
+
+    return dir;
+}
+
+/**
+ * @async
+ * @function onPackage
+ * @description run nsecure on a given npm package (on the npm registry).
+ * @param {!string} packageName
+ * @returns {Promise<string>}
+ */
+async function onPackage(packageName) {
+    await securityLock.acquireOne();
+
+    try {
+        const name = `${packageName}.json`;
+        const payload = await from(packageName, {
+            maxDepth: 4, verbose: false
+        });
+
+        const result = JSON.stringify(Object.fromEntries(payload), null, 2);
+        const filePath = join(JSON_DIR, name);
+        await mkdir(dirname(filePath), { recursive: true });
+        await writeFile(filePath, result);
+
+        return filePath;
+    }
+    catch (error) {
+        return null;
+    }
+    finally {
+        securityLock.freeOne();
+    }
+}
+
+/**
+ * @async
+ * @function onLocalDirectory
+ * @description run nsecure on a local directory
+ * @param {!string} dir
+ * @returns {Promise<string>}
+ */
+async function onLocalDirectory(dir) {
+    await securityLock.acquireOne();
+
+    try {
+        const name = `${basename(dir)}.json`;
+        const payload = await cwd(dir, {
+            maxDepth: 4, verbose: false
+        });
+
+        const result = JSON.stringify(Object.fromEntries(payload), null, 2);
+        const filePath = join(JSON_DIR, name);
+        await writeFile(filePath, result);
+
+        return filePath;
+    }
+    catch (error) {
+        return null;
+    }
+    finally {
+        securityLock.freeOne();
+    }
+}
+
+module.exports = {
+    linkPackages,
+    stats,
+    cloneGITRepository,
+    nsecure: Object.freeze({
+        onPackage, onLocalDirectory
+    })
+};
