c0a2b409faef377217bc7c8fe361f91284818e1b
halcin <hubert.alcin@gmail.com>
2023-05-22 13:59:49 +0200

    fix: missing labels in charts (#257) (#273)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-invalid-this */
 
 // Import Internal Dependencies
-import "../lib/chart.js";
 import { md5 } from "../lib/md5.js";
 
 // Import ESBUILD Assets
@@ -12,7 +11,15 @@ const kChartOptions = {
   legend: {
     display: false
   },
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  },
   plugins: {
+    legend: {
+      display: true
+    },
     datalabels: {
       anchor: "center",
       textShadowBlur: 4,
@@ -51,18 +58,28 @@ function* interpolateColors(dataLength, scale, range) {
 function createChart(elementId, type = "bar", payload = {}) {
   const { labels, data, interpolate = d3.interpolateCool } = payload;
   const options = JSON.parse(JSON.stringify(kChartOptions));
+  const chartType = (type === "horizontalBar") ? "bar" : type;
+  if (type === "horizontalBar") {
+    options.indexAxis = "y";
+  }
   if (type === "pie") {
     options.legend.display = true;
     options.plugins.datalabels.align = "center";
   }
   else {
+    options.plugins.legend.display = false;
     options.plugins.datalabels.align = type === "bar" ? "top" : "right";
   }
 
   new Chart(document.getElementById(elementId).getContext("2d"), {
-    type,
+    type: chartType,
+    plugins: [ChartDataLabels],
     data: {
+      label: "",
       labels,
+      legend: {
+        display: false
+      },
       datasets: [{
         borderWidth: 0,
         backgroundColor: [...interpolateColors(labels.length, interpolate, colorRangeInfo)],
@@ -85,10 +102,6 @@ function nodeDepClick() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  Chart.Legend.prototype.afterFit = function afterFit() {
-    this.height += 15;
-  };
-
   const avatarsElements = document.querySelectorAll(".avatar");
   for (const avatar of avatarsElements) {
     const email = avatar.getAttribute("data-email");
